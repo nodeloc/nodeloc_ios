4877902b3ca02ab47d7242be8c5f418a3acdd75b
nodeloc <james@nodeloc.com>
2026-08-19 11:33:52 +0800

    Initial Commit

---
+++ nodeloc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nodeloc
+//
+//  Created by Jungle on 2026/08/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nodeloc/nodelocApp.swift
@@ -0,0 +1,17 @@
+//
+//  nodelocApp.swift
+//  nodeloc
+//
+//  Created by Jungle on 2026/08/19.
+//
+
+import SwiftUI
+
+@main
+struct nodelocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
